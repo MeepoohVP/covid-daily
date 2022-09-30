4fcfaa5e732d1dda976518f0c63bde6e07b5483f
Pooh <poohvp1147@gmail.com>
2022-09-30 23:13:04 +0700

    80%

--- covid_API.js
+++ covid_API.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-all';
+/*const baseURL = 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-all';
 
 fetch(baseURL)
 
@@ -29,4 +29,86 @@ function appendData(data) {
     document.getElementById("total_death").innerHTML = '<strong>'+ totaldeath+'</strong>';
     document.getElementById("total_healing").innerHTML = '<strong>'+totalrecoverd+'</strong>';
     
+}*/
+
+//const country = prompt('Please enter your country','thailand');
+function covid(){
+const country = document.getElementById("country").value;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '42dc2f1aa5msh5bf1af0cc42368fp1f03c6jsn88c35a62d997',
+		'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+	}
+};
+function appendData(data){
+    const infect = data.response[0].cases['new'];
+    const death = data.response[0].deaths['new'];
+    const oneM = data.response[0].cases["1M_pop"];
+    const total_infect = data.response[0].cases['total'];
+    const total_death = data.response[0].deaths['total'];
+    const total_healing = data.response[0].cases['recovered'];
+    document.getElementById('infect').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>' + infect + '</strong>&nbsp;&nbsp;ราย';
+    document.getElementById('death').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>' + death + '</strong>&nbsp;&nbsp;ราย';
+    document.getElementById("healing").innerHTML = '&nbsp;&nbsp;<strong>'+(Number(oneM)).toLocaleString() + '</strong>&nbsp;&nbsp;ราย';
+    document.getElementById('total_infect').innerHTML = '<strong>'+(Number(total_infect)).toLocaleString()+'</strong>';
+    document.getElementById('total_death').innerHTML = '<strong>'+(Number(total_death)).toLocaleString()+'</strong>';
+    document.getElementById('total_healing').innerHTML = '<strong>'+(Number(total_healing)).toLocaleString()+'</strong>';
+    document.getElementById('date').innerHTML = data.response[0].day
+    if (infect==null){
+        document.getElementById('infect').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>No update</strong>'
+    }
+    if (death==null){
+        document.getElementById('death').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>No update</strong>'
+    }
+    if (oneM==null){
+        document.getElementById('healing').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>No update</strong>'
+    }
+    if (total_infect==null){
+        document.getElementById('total_infect').innerHTML = '<strong>No update</strong>'
+    }
+    if (total_death==null){
+        document.getElementById('total_death').innerHTML = '<strong>No update</strong>'
+    }
+    if (total_healing==null){
+        document.getElementById('total_healing').innerHTML = '<strong>No update</strong>'
+    }
+    
+}
+fetch('https://covid-193.p.rapidapi.com/statistics?country='+country, options)
+	.then(data => data.json())
+	.then(data => appendData(data))
+	.catch(err => Swal.fire({
+        icon: 'error',
+        title: 'Data not found!',
+        text: 'Country name may be wrong.',
+        footer: '<a href="country.txt" target = "_blank">You can enter country name from this document.</a>'
+      }));
+}
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '42dc2f1aa5msh5bf1af0cc42368fp1f03c6jsn88c35a62d997',
+		'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+	}
+};
+function appendData(data){
+    document.getElementById('infect').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>' + data.response[0].cases['new']+ '</strong>&nbsp;&nbsp;ราย';
+    document.getElementById('death').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>' + data.response[0].deaths['new']+ '</strong>&nbsp;&nbsp;ราย';
+    document.getElementById("healing").innerHTML = '&nbsp;&nbsp;<strong>'+(Number(data.response[0].cases["1M_pop"])).toLocaleString() + '</strong>&nbsp;&nbsp;ราย';
+    document.getElementById('total_infect').innerHTML = '<strong>'+(Number(data.response[0].cases['total'])).toLocaleString()+'</strong>';
+    document.getElementById('total_death').innerHTML = '<strong>'+(Number(data.response[0].deaths['total'])).toLocaleString()+'</strong>';
+    document.getElementById('total_healing').innerHTML = '<strong>'+(Number(data.response[0].cases['recovered'])).toLocaleString()+'</strong>';
+    document.getElementById('date').innerHTML = data.response[0].day
+    
+}
+fetch('https://covid-193.p.rapidapi.com/statistics?country=thailand', options)
+	.then(data => data.json())
+	.then(data => appendData(data))
+	.catch(err => console.error(err));
+
+function search() {
+    if(event.key === 'Enter') {
+        covid();      
+    }
 }
--- translate.js
+++ translate.js
@@ -1,19 +1,17 @@
 function Translate_ENG() {
     document.getElementById('trans').innerHTML = '<button class="thais" id="thai" onclick="Translate_TH()">TH</button><button class="eng" id="eng" onclick="Translate_ENG()" style="background:none;border: 1px solid white;color: white;opacity:50%;width:35px;height:30px;box-shadow: inset 0 0 0 0 #50c878;cursor:default;">ENG</button>'
-    document.getElementById('topic').innerHTML = '<strong>Covid-19 update  <span id="date"></span></strong>';
     document.getElementById('cases').innerHTML = 'Cases';
     document.getElementById('deaths').innerHTML = 'Deaths';
-    document.getElementById('recovered').innerHTML = 'Recoverd';
+    document.getElementById('recovered').innerHTML = '1 months cases';
     document.getElementById('total_cases').innerHTML = 'Total cases';
     document.getElementById('total_deaths').innerHTML = 'Total deaths';
     document.getElementById('total_recoverd').innerHTML = 'Total recovered';
 }
 function Translate_TH(){
     document.getElementById('trans').innerHTML = '<button class="thai" id="thai" onclick="Translate_TH()" style="background:none;border: 1px solid white;color: white;opacity:50%;">TH</button><button class="engs" id="eng" onclick="Translate_ENG()">ENG</button>'
-    document.getElementById('topic').innerHTML = '<strong>รายงานสถานการณ์ covid-19  <span id="date"></span></strong>';
     document.getElementById('cases').innerHTML = 'ติดเชื้อ';
     document.getElementById('deaths').innerHTML = 'เสียชีวิต';
-    document.getElementById('recovered').innerHTML = 'หายป่วย&nbsp;&nbsp;&nbsp;';
+    document.getElementById('recovered').innerHTML = 'ป่วยสะสม 1 เดือน&nbsp;';
     document.getElementById('total_cases').innerHTML = 'ติดเชื้อสะสม';
     document.getElementById('total_deaths').innerHTML = 'เสียชีวิตสะสม';
     document.getElementById('total_recoverd').innerHTML = 'หายป่วยสะสม';
